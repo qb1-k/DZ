fbd8dfbcd382d23c7af45298d3584039df5afe36
qb1-k <vkaleksei4@gmail.com>
2024-11-01 20:16:04 +0300

    Add files via upload

---
+++ dz.1.6.2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath>
+
+int func(int value, int power)
+{
+	setlocale(LC_ALL, "RUS");
+	int result = pow(value,power);
+	std::cout << value << " в степени " << power << " = " << result << std::endl;
+	return result;
+}
+
+
+int main(int argc, char** argv)
+{
+	func(5, 2);
+	func(3, 3);
+	func(4, 4);
+	return 0;
+}
---
+++ dz.1.6.3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+void func1(unsigned long int a, unsigned long int b, int lim)
+{
+    if (lim == 0) { return; }
+    std::cout << a << " ";
+    func1(b, a + b, lim - 1);
+}
+
+void func(int n)
+{
+    func1(1, 1, n);
+}
+
+int main() {
+    int k{};
+    setlocale(LC_ALL, "RUS");
+    std::cout << " Введите число > 0: ";
+    std::cin >> k;
+    func(k);
+    return 0;
+}
