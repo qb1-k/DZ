b307ced176317adba55b7c9a6068656487080cea
qb1-k <vkaleksei4@gmail.com>
2024-11-19 21:41:16 +0300

    Add files via upload

---
+++ dz.1.11.1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string firstName{};
+	std::string lastName{};
+	std::cout << "Enter your last name: ";
+	std::getline(std::cin >> std::ws, lastName);
+	std::cout << "Enter your name:: ";
+	std::getline(std::cin >> std::ws, firstName);
+
+	std::string fullName = firstName + " " + lastName;
+	std::cout << "Hi " << fullName;
+
+	return 0;
+}
---
+++ dz.1.11.2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string word{ "apple" };
+	std::string enteredWord{};
+
+	do
+	{
+		std::cout << "Guess the fruit!" << std::endl;
+		std::getline(std::cin >> std::ws, enteredWord);
+		if (word != enteredWord)
+		{
+			std::cout << "Wrong!" << std::endl;
+		}
+	} while (word != enteredWord);
+
+	std::cout << "Right! You have won! Hidden word - " << word << std::endl;
+
+
+	return 0;
+}
