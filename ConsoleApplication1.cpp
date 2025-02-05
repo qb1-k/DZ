848df801cfb2b5861ede132ca209b74991d4f84e
qb1-k <vkaleksei4@gmail.com>
2025-02-05 22:17:57 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+enum Months {
+	January = 1,
+	February,
+	March,
+	April,
+	May,
+	June,
+	July,
+	August,
+	September,
+	October,
+	November,
+	December
+};
+
+int main()
+{
+	int number{};
+	do {
+		std::cout << "Enter month number: " << std::endl;
+		std::cin >> number;
+
+		Months n = static_cast<Months>(number);
+
+		switch (n) {
+		case January:
+			std::cout << "January" << std::endl;
+			break;
+		case February:
+			std::cout << "February" << std::endl;
+			break;
+		case March:
+			std::cout << "March" << std::endl;
+			break;
+		case April:
+			std::cout << "April" << std::endl;
+			break;
+		case May:
+			std::cout << "May" << std::endl;
+			break;
+		case June:
+			std::cout << "June" << std::endl;
+			break;
+		case July:
+			std::cout << "July" << std::endl;
+			break;
+		case August:
+			std::cout << "August" << std::endl;
+			break;
+		case September:
+			std::cout << "September" << std::endl;
+			break;
+		case October:
+			std::cout << "October" << std::endl;
+			break;
+		case November:
+			std::cout << "November" << std::endl;
+			break;
+		case December:
+			std::cout << "December" << std::endl;
+			break;
+		
+		};
+	} while (number > 0 && number < 13);
+
+	if (number > 12 || number < 0) {
+		std::cout << "Incorrect number";
+	}
+	if (number == 0) {
+		std::cout << "Goodbye";
+	}
+	return 0;
+}
