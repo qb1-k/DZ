0cd7ce0beff2aa4711860a36f450ed516e8a74f5
qb1-k <vkaleksei4@gmail.com>
2025-02-10 18:00:31 +0300

    Add files via upload

---
+++ dz.3.4.1.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+
+
+class Figure
+{
+private:
+    std::string name; // название фигуры
+    int num; // количество сторон
+
+public:
+    Figure(std::string name, int num) {
+        this->name = name;
+        this->num = num;
+    }
+    void get_sides_count() {
+        std::cout << name << num << "\n";
+    }
+    Figure() {
+        this->name = "Фигура: ";
+        this->num = 0;
+    }
+};
+
+
+class Triangle : public Figure
+{
+public:
+    Triangle(std::string name, int num) : Figure{ name, num } {}
+};
+
+class Quadrangle : public Figure
+{
+public:
+    Quadrangle(std::string name, int num) : Figure{ name, num } {}
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    Figure f;
+    f.get_sides_count();
+
+    Triangle f1("Треугольник: ", 3);
+    f1.get_sides_count();
+
+    Quadrangle f2("Четырехугольник: ", 4);
+    f2.get_sides_count();
+}
